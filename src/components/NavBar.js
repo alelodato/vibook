d2b138415a17f4fc6c5ccce1a94d52e4fe9cb32b
alelodato <alss.ldt@gmail.com>
2025-05-03 10:20:17 +0200

    added favicon, logo and signup link to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from '../assets/logo.png'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
+    <Navbar expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand href="#home"><img src={logo} alt="logo" height="45" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link>Home</Nav.Link>
+            <Nav.Link>SignIn</Nav.Link>
+            <Nav.Link>SignUp</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
-export default NavBar
+export default NavBar;
