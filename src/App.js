a8247aee23626b06923a18c60020f83b41a9cd5e
alelodato <alss.ldt@gmail.com>
2025-05-03 11:02:25 +0200

    added router to index.js, routes to app.js and navlinks to navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import { Container } from 'react-bootstrap';
+import { Route,Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
-        <h1>Home Page</h1>
-        <h1>Sign In</h1>
+        <Switch>
+          <Route exact path='/' render={() => <h1>Home Page</h1>} />
+          <Route exact path='/signin' render={() => <h1>Sign In</h1>} />
+          <Route exact path='/signup' render={() => <h1>Sign Up</h1>} />
+        </Switch>
       </Container>
     </div>
   );
